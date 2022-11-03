b3f6c3e7b8b2d9cd28ef53c3b819e90a8d85d6ab
qyrBalasy <104926205+QyrBalasy@users.noreply.github.com>
2022-11-03 14:19:34 +0600

    init: gulp init full

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,5 +1,5 @@
 // Получаем имя папки нашего проекта
-import * as nodePath from 'path'; // импортируем модуль
+import * as nodePath from "path"; // импортируем модуль
 const rootFolder = nodePath.basename(nodePath.resolve());
 
 const buildFolder = `./dist`; //автоматом создается папка
@@ -7,15 +7,27 @@ const srcFolder = `./src`;
 
 export const path = {
   build: {
+    js: `${buildFolder}/js/`,
+    css: `${buildFolder}/css/`,
     html: `${buildFolder}/`,
+    images: `${buildFolder}/img/`,
+    fonts: `${buildFolder}/fonts/`,
     files: `${buildFolder}/files/`,
   },
   src: {
+    js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
+    scss: `${srcFolder}/scss/style.scss`,
     html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`,
+    svgicons: `${srcFolder}/svgicons/*.svg`,
   },
   watch: {
+    js: `${srcFolder}/js/**/*.js`,
+    scss: `${srcFolder}/scss/**/*.scss`,
     html: `${srcFolder}/**/*.html`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
     files: `${srcFolder}/files/**/*.*`,
   },
   clean: buildFolder,
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+// плагины через npm устанавливаешь и модули вызываешь
+// import Swiper, { Navigation, Pagination } from "swiper";
+// const swiper = new Swiper();
