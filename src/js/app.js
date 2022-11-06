571c94d9280d4fa9b4b93aacaf57d39a1c2b20c4
qyrBalasy <104926205+QyrBalasy@users.noreply.github.com>
2022-11-06 20:21:27 +0600

    feat: add phone input with mask

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
 import * as flsFunctions from "./modules/functions.js";
+import * as phoneInput from './modules/phoneinput.js';
 
 flsFunctions.isWebp();
+phoneInput.isPhone();
 
 // плагины через npm устанавливаешь и модули вызываешь
 // import Swiper, { Navigation, Pagination } from "swiper";
 // const swiper = new Swiper();
+
+
