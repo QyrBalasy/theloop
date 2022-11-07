87300d228af9937d64d8aedcad2079077ce5e978
qyrBalasy <104926205+QyrBalasy@users.noreply.github.com>
2022-11-07 19:44:46 +0600

    feat: add phoneinput mask

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as accordion from "./modules/accordion.js";
+import * as phoneInput from "./modules/phoneinput.js";
 
 flsFunctions.isWebp();
 accordion.showContent();
+phoneInput.isPhone();
 
 // плагины через npm устанавливаешь и модули вызываешь
 // import Swiper, { Navigation, Pagination } from "swiper";
