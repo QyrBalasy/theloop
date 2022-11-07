177b32d4cacc69f3ba6dca3e557490ae12629f82
qyrBalasy <104926205+QyrBalasy@users.noreply.github.com>
2022-11-07 19:37:53 +0600

    feat: add section three

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import * as flsFunctions from "./modules/functions.js";
+import * as accordion from "./modules/accordion.js";
 
 flsFunctions.isWebp();
+accordion.showContent();
 
 // плагины через npm устанавливаешь и модули вызываешь
 // import Swiper, { Navigation, Pagination } from "swiper";
