2abcc371b53bdff3146b4b238171f0dd998b8ece
qyrBalasy <104926205+QyrBalasy@users.noreply.github.com>
2022-11-07 22:09:50 +0600

    Update 2022-11-07T16:09:32.310Z

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import * as flsFunctions from "./modules/functions.js";
-
-flsFunctions.isWebp();
-
-// плагины через npm устанавливаешь и модули вызываешь
-// import Swiper, { Navigation, Pagination } from "swiper";
-// const swiper = new Swiper();
