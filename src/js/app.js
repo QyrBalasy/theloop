7364e4cd17f6c6ae6ab48707535861918e315299
qyrBalasy <104926205+QyrBalasy@users.noreply.github.com>
2022-11-14 16:09:25 +0600

    feat: add slider

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,29 @@ accordion.showContent();
 phoneInput.isPhone();
 
 // плагины через npm устанавливаешь и модули вызываешь
-// import Swiper, { Navigation, Pagination } from "swiper";
-// const swiper = new Swiper();
+import Swiper, { Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation, Pagination],
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
