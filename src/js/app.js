e2cb7eadb994a4072aa95a5d481f7a7751362edf
qyrBalasy <104926205+QyrBalasy@users.noreply.github.com>
2022-11-30 10:02:18 +0600

    feat: add sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,38 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as accordion from "./modules/accordion.js";
 import * as phoneInput from "./modules/phoneinput.js";
+import {MySwiper}from './modules/swiper.js';
 
 flsFunctions.isWebp();
 accordion.showContent();
 phoneInput.isPhone();
+MySwiper();
 
-// плагины через npm устанавливаешь и модули вызываешь
-import Swiper, { Navigation, Pagination } from "swiper";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-const swiper = new Swiper(".swiper", {
-  modules: [Navigation, Pagination],
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
+// // плагины через npm устанавливаешь и модули вызываешь
+// import Swiper, { Navigation, Pagination } from "swiper";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+// const swiper = new Swiper(".swiper", {
+//   modules: [Navigation, Pagination],
+//   // Optional parameters
+//   direction: "horizontal",
+//   loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+// });
